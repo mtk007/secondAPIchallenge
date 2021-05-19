aa12d27803bb2e826d4bd865166ca53421d9c317
Matt's M1 2021 <mattm1@Mattss-MacBook-Pro.local>
2021-05-19 00:06:03 -0400

    added fetch iterations

--- public/pollution.js
+++ public/pollution.js
@@ -1,20 +1,21 @@
 const display = () => {}
-let data = []
+let data = [];
 
-//fetch('https://api.airvisual.com/v2/nearest_station?lat={{39}}&lon={{86}}&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
 
-  fetch('https://api.airvisual.com/v2/cities?state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+fetch('https://api.airvisual.com/v2/nearest_city?key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+
+  //fetch('https://api.airvisual.com/v2/cities?state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
   
-  fetch('https://api.airvisual.com/v2/Brownsburg?lat=39.8434&lon=86.3978&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+//  fetch('https://api.airvisual.com/v2/Brownsburg?lat=39.8434&lon=86.3978&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
   
   .then(function (response) {
     return response.json()
   })
   .then(
     function (results) {
-      //  console.log(results.data)
+       console.log(results.data)
       //  return results.data 
-      displayBrownsburg(results.data)      
+      displayCities(results.data)      
     },
   )
     function displayCities(items) {
@@ -24,65 +25,43 @@ let data = []
         
       }); 
       console.log(data);
+    };
 
-     fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a') 
-      .then(function (response) {
+fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a') 
+            .then(function (response) {
+             return response.json()
+                 })
+                .then(
+                function (results) {
+                    console.log(results.data)
+                displayIndiana(results.data2)      
+                },
+                    )
+      function displayIndiana(items) {
+        console.log(items)
+        
+        data = items.forEach((city2) => {
+          data.push(city2)
+          
+        }); 
+        console.log(city2);
+      };
+
+fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+     .then(function (response) {
         return response.json()
       })
       .then(
         function (results) {
-          //  console.log(results.data)
-          //  return results.data 
-          displayBrownsburg(results.data)      
+            console.log(results.data)
+          displayBrownsburg(results.data3)      
         },
       )
-        function displayCities(items) {
+        function displayBrownsburg(items) {
           console.log(items)
-          data = items.forEach((city) => {
-            data.push(city)
+          data = items.forEach((city3) => {
+            data.push(city3)
             
           }); 
-          console.log(data);
-
-        fetch('https://api.airvisual.com/v2/Brownsburg?lat=39.8434&lon=86.3978&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
-
-      /*let table = document.createElement('table')
-      let row = document.createElement('tr')
-      let th1 = document.createElement('th')
-      let th2 = document.createElement('th')
-      let th3 = document.createElement('th')
-      let th4 = document.createElement('th')
-      section.appendChild(table)
-      table.appendChild(row)
-      row.appendChild(th1)
-      row.appendChild(th2)
-      row.appendChild(th3)
-      row.appendChild(th4)
-      th1.innerHTML = 'city'
-      th2.innerHTML = 'location'
-      th3.innerHTML = 'aqius'
-      th4.innerHTML = 'pollution'
-
-      items.forEach((data) => {
-        let dRow = document.createElement('tr')
-        table.appendChild(dRow)
-
-        let iCity = document.createElement('td')
-
-        let iLocation = document.createElement('td')
-
-        let iPollution = document.createElement('td')
-
-        dRow.appendChild(iCity)
-        dRow.appendChild(iLocation)
-        dRow.appendChild(iAQIUS)
-        dRow.appendChild(iPollution)
-
-        ;(iCity.innerHTML = data.city),
-          (iLocation.innerHTML = data.location),
-          (iCurrent.innerHTML = data.current),
-          (iPollution.innerHTML = data.pollution)
-
-        console.log(iCity)
-      })
-    }
+          console.log(data3);
+        };
