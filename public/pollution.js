2458d82dd01e5df6db8a2d77be86ae1a2d1f378b
Matt's M1 2021 <mattm1@Mattss-MacBook-Pro.local>
2021-05-19 17:03:48 -0400

    message v0.6

--- public/pollution.js
+++ public/pollution.js
@@ -3,11 +3,6 @@ let data = [];
 
 
 fetch('https://api.airvisual.com/v2/nearest_city?key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
-
-  //fetch('https://api.airvisual.com/v2/cities?state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
-  
-//  fetch('https://api.airvisual.com/v2/Brownsburg?lat=39.8434&lon=86.3978&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
-  
   .then(function (response) {
     return response.json()
   })
@@ -15,16 +10,15 @@ fetch('https://api.airvisual.com/v2/nearest_city?key=3e48273e-816e-40dc-bc0c-a40
     function (results) {
        console.log(results.data)
       //  return results.data 
-      displayCities(results.data)      
+      displayIndy(results.data)      
     },
   )
-    function displayCities(items) {
-      console.log(items)
+    function displayIndy(items) {
       data = items.forEach((city) => {
         data.push(city)
         
       }); 
-      console.log(data);
+      console.log(city);
     };
 
 fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a') 
@@ -34,11 +28,10 @@ fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=U
                 .then(
                 function (results) {
                     console.log(results.data)
-                displayIndiana(results.data2)      
+                displayBrownsburg(results.data2)      
                 },
                     )
-      function displayIndiana(items) {
-        console.log(items)
+      function displayBrownsburg(items) {
         
         data = items.forEach((city2) => {
           data.push(city2)
@@ -47,7 +40,7 @@ fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=U
         console.log(city2);
       };
 
-fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+fetch('https://api.airvisual.com/v2/city?city=Fishers&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
      .then(function (response) {
         return response.json()
       })
@@ -63,5 +56,45 @@ fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=U
             data.push(city3)
             
           }); 
-          console.log(data3);
+          console.log(city3);
         };
+
+fetch('https://api.airvisual.com/v2/city?city=Greencastle&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+        .then(function (response) {
+           return response.json()
+         })
+         .then(
+           function (results) {
+               console.log(results.data)
+             displayBrownsburg(results.data3)      
+           },
+         )
+           function displayBrownsburg(items) {
+             console.log(items)
+             data = items.forEach((city4) => {
+               data.push(city4)
+               
+             }); 
+             console.log(city4);
+           };
+
+  fetch('https://api.airvisual.com/v2/cities?state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+
+  .then(function (response) {
+    return response.json()
+  })
+  .then(
+    function (results) {
+        console.log(results.data)
+      displayAll(results.data3)      
+    },
+  )
+    function displayAll(items) {
+      console.log(items)
+      data = items.forEach((allCities) => {
+        data.push(allCities)
+        
+      }); 
+      console.log(allCities);
+    };
+
