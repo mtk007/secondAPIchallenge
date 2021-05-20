7b780755721a20676300d6182bc76acd8a5e1c9f
Matt's M1 2021 <mattm1@Mattss-MacBook-Pro.local>
2021-05-19 23:57:23 -0400

    pollution data printed

--- public/pollution.js
+++ public/pollution.js
@@ -1,8 +1,144 @@
 const display = () => {}
 let data = [];
+let results = document.getElementById('results')
+
+//brownsburg
+fetch("https://airvisual1.p.rapidapi.com/cities/get-information?id=5bc822aa41fdcdf1939e6485", {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72",
+    "x-rapidapi-host": "airvisual1.p.rapidapi.com"
+              }
+            })
+            .then(response => {
+              return response.json()
+            })
+            .then(
+              function (brownsburg) {
+                displayCity(brownsburg) 
+              }
+            )
+            .catch(err => {
+              console.error(err);
+            });
 
 
-fetch('https://api.airvisual.com/v2/nearest_city?key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+//clermont
+           fetch("https://airvisual1.p.rapidapi.com/cities/get-information?id=5bc822aa41fdcdf1939e648d", {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-key": "3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72",
+              "x-rapidapi-host": "airvisual1.p.rapidapi.com"
+            }
+          })
+          .then(response => {
+            return response.json()
+          })
+          .then(
+            function (clermont) {
+              displayCity(clermont)   
+            }
+          )
+          .catch(err => {
+            console.error(err);
+          });
+
+  
+//speedway
+          fetch("https://airvisual1.p.rapidapi.com/cities/get-information?id=5bc822ac41fdcdf1939e64fc", {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-key": "3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72",
+              "x-rapidapi-host": "airvisual1.p.rapidapi.com"
+            }
+          })
+          .then(response => {
+            return response.json()
+          })
+          .then(
+            function (speedway) {
+              displayCity(speedway)    
+            }
+          )
+          .catch(err => {
+            console.error(err);
+          });
+
+//zionsville
+          fetch("https://airvisual1.p.rapidapi.com/cities/get-information?id=5bc822ac41fdcdf1939e650a", {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-key": "3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72",
+              "x-rapidapi-host": "airvisual1.p.rapidapi.com"
+            }
+          })
+          .then(response => {
+            return response.json()
+          })
+          .then(
+            function (zionsville) {
+              displayCity(zionsville)   
+            }
+          )
+          .catch(err => {
+            console.error(err);
+          });
+
+let displayCity=(weatherdata) => {
+      console.log(weatherdata.data.city);
+      console.log(weatherdata.data.current_measurement.aqius);
+      console.log(weatherdata.data.current_weather.wd);
+      console.log(weatherdata.data.current_weather.ws);
+      let parent = document.createElement('div');  
+      let cityName = document.createElement('p');  
+      let airQuality = document.createElement('a');
+      let windSpeed = document.createElement('s');
+      let windDirection = document.createElement('d');
+      cityName.innerHTML = (weatherdata.data.city);
+      airQuality.innerHTML = (weatherdata.data.current_measurement.aqius);
+      windSpeed.innerHTML = (weatherdata.data.current_weather.ws);
+      windDirection.innerHTML = (weatherdata.data.current_weather.wd);
+      parent.appendChild(cityName);
+      results.appendChild(parent);
+      parent.appendChild(airQuality);
+      results.appendChild(parent);
+      parent.appendChild(windSpeed);
+      results.appendChild(parent);
+      parent.appendChild(windDirection);
+      results.appendChild(parent);
+    };
+
+
+
+
+//general Pro query
+          /*fetch("https://airvisual1.p.rapidapi.com/auto-complete?query=indianapolis", {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-key": "3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72",
+              "x-rapidapi-host": "airvisual1.p.rapidapi.com"
+            }
+          })
+          .then(response => {
+            return response.json()
+          })
+          .then(
+            function (results) {
+              console.log(results.data);
+            }
+          )
+          .catch(err => {
+            console.error(err);
+          });*/
+
+
+
+// free API key configs below
+          /***************************************************************
+          *************************************************************/
+
+
+/*fetch('https://api.airvisual.com/v2/nearest_city?key=3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72')
   .then(function (response) {
     return response.json()
   })
@@ -21,7 +157,7 @@ fetch('https://api.airvisual.com/v2/nearest_city?key=3e48273e-816e-40dc-bc0c-a40
       console.log(city);
     };
 
-fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a') 
+fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=USA&key=3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72') 
             .then(function (response) {
              return response.json()
                  })
@@ -40,14 +176,14 @@ fetch('https://api.airvisual.com/v2/city?city=Brownsburg&state=Indiana&country=U
         console.log(city2);
       };
 
-fetch('https://api.airvisual.com/v2/city?city=Fishers&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
+fetch('https://api.airvisual.com/v2/city?city=Fishers&state=Indiana&country=USA&key=33ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72')
      .then(function (response) {
         return response.json()
       })
       .then(
         function (results) {
             console.log(results.data)
-          displayBrownsburg(results.data3)      
+          displayBrownsburg(results.data)      
         },
       )
         function displayBrownsburg(items) {
@@ -59,8 +195,8 @@ fetch('https://api.airvisual.com/v2/city?city=Fishers&state=Indiana&country=USA&
           console.log(city3);
         };
 
-fetch('https://api.airvisual.com/v2/city?city=Greencastle&state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
-        .then(function (response) {
+//fetch('https://api.airvisual.com/v2/city?city=Greencastle&state=Indiana&country=USA&key=3ec19f0389msh7b3a40819ec9bfep18bdedjsnf2a96f4f0c72')
+     .then(function (response) {
            return response.json()
          })
          .then(
@@ -76,25 +212,4 @@ fetch('https://api.airvisual.com/v2/city?city=Greencastle&state=Indiana&country=
                
              }); 
              console.log(city4);
-           };
-
-  fetch('https://api.airvisual.com/v2/cities?state=Indiana&country=USA&key=3e48273e-816e-40dc-bc0c-a40ef1e4a37a')
-
-  .then(function (response) {
-    return response.json()
-  })
-  .then(
-    function (results) {
-        console.log(results.data)
-      displayAll(results.data3)      
-    },
-  )
-    function displayAll(items) {
-      console.log(items)
-      data = items.forEach((allCities) => {
-        data.push(allCities)
-        
-      }); 
-      console.log(allCities);
-    };
-
+           }; */
